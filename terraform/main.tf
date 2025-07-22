d3a5701b9c442787a2dac76e6dfdbdb8c5d46440
Nelson Holanda <nelsonholanda@hotmail.com>
2025-07-22 13:23:53 -0300

    update tfstate s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  
+  backend "s3" {
+    bucket = "tf-nh"
+    key    = "kiro-tf-bia/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
